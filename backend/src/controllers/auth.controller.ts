02f88fee82bd6e53c62294e02440791fa6e78444
JohnAndante <walker_contas@hotmail.com>
2025-09-26 01:55:10 -0300

    #0 - fix: Removed unused params

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,6 +1,6 @@
 import bcrypt from 'bcryptjs';
 import { Request, Response } from 'express';
-import { generateToken, AuthenticatedRequest } from '../utils/auth';
+import { AuthenticatedRequest } from '../utils/auth';
 import { authService } from '../services/auth.service';
 
 export class AuthController {
