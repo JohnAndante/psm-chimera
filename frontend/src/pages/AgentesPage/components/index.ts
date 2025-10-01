879829599c893b74217ed6f24f9e9ad214749e85
JohnAndante <walker_contas@hotmail.com>
2025-09-30 21:17:27 -0300

    #0 - refactor: Removed unused files

--- frontend/src/pages/AgentesPage/components/index.ts
+++
@@ -1,7 +0,0 @@
-// ===========================
-// Agentes Page Components
-// ===========================
-
-export { AgentsList } from './agents-list'
-export { AgentForm } from './agent-form'
-export { AgentView } from './agent-view'
