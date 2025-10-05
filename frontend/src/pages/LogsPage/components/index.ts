0592454509d1b1c6ae387acee5879a4c8c70aa1e
Walker Silvestre <walker_contas@hotmail.com>
2025-10-05 20:24:13 -0300

    #0 - refactor: remove old LogsPage components and replace with new implementation pattern

--- frontend/src/pages/LogsPage/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { LogViewer } from './log-viewer';
-export { LogFilters } from './log-filters';
-export { LogStatisticsCard } from './log-statistic-card';
-export { LogSessionList } from './log-session-list';
